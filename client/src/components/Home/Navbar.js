a12cac3fee5b40f1875289674a4a1ebdbf9756f7
rohith-21 <55896422+rohith-21@users.noreply.github.com>
2023-11-29 02:26:50 +0530

    final commit

--- client/src/components/Home/Navbar.js
+++ client/src/components/Home/Navbar.js
@@ -9,26 +9,7 @@ function Navbar() {
           <a href="/" id="nav-logo">AirConf</a>
           <div id="menu">
             <ul className="menu-list">
-              <li className="menu-item">
-                <a href="https://github.com/FaizBShah" target="_blank" rel="noreferrer">
-                  <i className="fab fa-github"></i>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="https://www.linkedin.com/in/faiz-shah-0b5955189/" target="_blank" rel="noreferrer">
-                  <i className="fab fa-linkedin"></i>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="https://twitter.com/Random_CSE_guy" target="_blank" rel="noreferrer">
-                  <i className="fab fa-twitter"></i>
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="mailto:faizbshah2001@gmail.com" target="_blank" rel="noreferrer">
-                  <i className="fas fa-envelope"></i>
-                </a>
-              </li>
+              
             </ul>
           </div>
         </div>
